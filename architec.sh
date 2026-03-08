0442efbe35362ce8eef4c0fb43b55ccf85524229
0x78 <98791660+NikiforosVasilakis@users.noreply.github.com>
2026-03-08 14:46:57 +0200

    Create directories for media and config files

---
+++ architec.sh
@@ -0,0 +1,13 @@
+mkdir -p \
+config/jellyfin \
+config/jellyseerr \
+config/sonarr \
+config/radarr \
+config/prowlarr \
+config/bazarr \
+config/qbittorrent \
+media/movies \
+media/tv \
+media/anime \
+torrents/movies \
+torrents/tv
